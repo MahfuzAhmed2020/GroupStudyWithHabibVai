fc86c126b7e70fd4348a50dd1450938ba2df9ea6
mahfuz <20mahfuzahmed20@gmail.com>
2021-06-23 22:50:12 -0400

    adding main method

--- src/main/java/com/mahfuz/group/GroupMahfuz.java
+++ src/main/java/com/mahfuz/group/GroupMahfuz.java
@@ -1,5 +1,8 @@
 package com.mahfuz.group;
 
 public class GroupMahfuz {
+	public static void main(String[] args) {
+		System.out.println("System.currentTimeMillis() = "+System.currentTimeMillis()+"\nSystem.getProperty(\"os.name\") = " +System.getProperty("os.name")+"\nSystem.getProperty(\"user.dir\") = "+System.getProperty("user.dir"));
+	}
 
 }
